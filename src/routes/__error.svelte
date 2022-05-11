6897fbaf82106bffbbf9293ab9668a3715ecd4ce
Brittney Postma <brittneypostma@Brittneys-MacBook-Pro.local>
2022-05-11 14:46:01 -0400

    error catch route

---
+++ src/routes/__error.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				error: `${status}: ${error?.message}`
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let error: string;
+</script>
+
+<h1>{error}</h1>
